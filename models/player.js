1babe95de9927ed2863da77f9542d71fcb6c764a
Christoph Federwisch <christoph.federwisch@thinxnet.com>
2014-12-31 00:40:01 +0100

    unique index for players

--- models/player.js
+++ models/player.js
@@ -4,10 +4,16 @@ var Schema = mongoose.Schema;
 var playerSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   }
 
   //TODO: create index on name
 });
+
+playerSchema.index({name: 1, type: -1});
+
+// uncomment in production
+// playerSchema.set('autoIndex', false);
 
 module.exports = mongoose.model('Player', playerSchema);
